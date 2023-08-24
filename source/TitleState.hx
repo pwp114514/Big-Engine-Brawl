2028e33019eea7fa6c837dfbd64aaa795f544928
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-08-24 14:50:12 +0800

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -38,10 +38,7 @@ import flixel.util.FlxTimer;
 import openfl.Assets;
 
 #if VIDEOS_ALLOWED
-#if (hxCodec >= "2.6.1") import hxcodec.VideoHandler as MP4Handler;
-#elseif (hxCodec == "2.6.0") import VideoHandler as MP4Handler;
-#else import vlc.MP4Handler; #end
-import hxcodec.VideoSprite;
+import VideoSprite as VideoHandler;
 #end
 
 using StringTools;
